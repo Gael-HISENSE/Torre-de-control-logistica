a15d4f44f057ffeab0aab07be9fd3ba0133ce345
Gael-HISENSE <gael.gruposid@gmail.com>
2024-08-03 14:15:24 -0600

    styles.css

---
+++ styles.css
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LogisticsControlTower from './App'; // Asegúrate de que la ruta sea correcta
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LogisticsControlTower />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
